4d1d60d84a5bf3f8fecbf2511eb9d0eccf97d4c3
marco <odri.fo@gmail.com>
2020-11-20 17:59:12 +0100

    incremento js

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const myApp = new Vue({
+  el:'#myBoolzapp',
+  data:{
+    nomeUtente:{
+      nome: 'myName',
+      image: 'img/avatar_io.jpg'
+    },
+    contacts:[
+      {
+        name: 'michele',
+        image: 'img/avatar_1.jpg',
+        lastChat:'03/10/20'
+      },
+      {
+        name: 'rosa',
+        image: 'img/avatar_2.jpg',
+        lastChat:'03/10/20'
+      },
+      {
+        name: 'giovanni',
+        image: 'img/avatar_3.jpg',
+        lastChat:'03/10/20'
+      },
+      {
+        name: 'maria',
+        image: 'img/avatar_4.jpg',
+        lastChat:'03/10/20'
+      },
+      {
+        name: 'pablo',
+        image: 'img/avatar_5.jpg',
+        lastChat:'03/10/20'
+      },
+      {
+        name: 'luisa',
+        image: 'img/avatar_6.jpg',
+        lastChat:'03/10/20'
+      }
+    ]
+  }
+})
